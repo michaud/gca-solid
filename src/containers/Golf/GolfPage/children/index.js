aca00081e6503c769bd2178c3dd29262fbc9549f
Michaud <michaud@venant.nl>
2019-12-26 20:49:59 +0100

    update exports

--- src/containers/Golf/GolfPage/children/index.js
+++ src/containers/Golf/GolfPage/children/index.js
@@ -1,13 +1,59 @@
+import BagTransferList from './BagTransferList';
+import ButtonBar from './ButtonBar';
 import ClubDetail from './ClubDetail';
+import clubDetailStyles from './clubDetail.style';
 import ClubForm from './ClubForm';
+import ClubList from './ClubList';
 import ClubTypeSelector from './ClubTypeSelector';
+import CourseDetail from './CourseDetail';
+import CourseForm from './CourseForm';
+import CourseHoleTable from './CourseHoleTable';
+import CourseList from './CourseList';
+import formStyles from './form.style';
+import GolfApp from './GolfApp';
+import HoleDetail from './HoleDetail';
+import HoleForm from './HoleForm';
+import HoleList from './HoleList';
 import IDEditor from './IDEditor';
 import ManageBag from './ManageBag';
+import ManageCourses from './ManageCourses';
+import ManageHoles from './ManageHoles';
+import ManageMarkers from './ManageMarkers';
+import ManagePlayers from './ManagePlayers';
+import MarkerList from './MarkerList';
+import ModuleHeader from './ModuleHeader';
+import NavigationShell from './NavigationShell';
+import PlayerDetail from './PlayerDetail';
+import PlayerForm from './PlayerForm';
+import SplashScreen from './SplashScreen';
 
 export {
+    BagTransferList,
+    ButtonBar,
     ClubDetail,
+    clubDetailStyles,
     ClubForm,
+    ClubList,
     ClubTypeSelector,
+    CourseDetail,
+    CourseForm,
+    CourseHoleTable,
+    CourseList,
+    formStyles,
+    GolfApp,
+    HoleDetail,
+    HoleForm,
+    HoleList,
     IDEditor,
-    ManageBag
-};
+    ManageBag,
+    ManageCourses,
+    ManageHoles,
+    ManageMarkers,
+    ManagePlayers,
+    MarkerList,
+    ModuleHeader,
+    NavigationShell,
+    PlayerDetail,
+    PlayerForm,
+    SplashScreen
+}
