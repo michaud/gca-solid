21a4a008bbb692fe9d8057da4f288d8b0d7201ce
Michaud <michaud@venant.nl>
2020-01-22 18:14:52 +0100

    fix: use correct command for array

--- src/containers/Golf/GolfPage/children/course/CourseForm.js
+++ src/containers/Golf/GolfPage/children/course/CourseForm.js
@@ -37,7 +37,7 @@ const CourseForm = ({
     const onAddHole = (hole) => {
         
         setCourseState(state => update(state, {
-            courseHoles: { value: { $push: hole } }
+            courseHoles: { value: { $push: [hole] } }
         }));
     };
 
