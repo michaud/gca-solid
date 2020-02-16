8b495c2399210f50c91bf99202249ada7c5f17ee
Michaud <michaud@venant.nl>
2020-02-16 12:06:06 +0100

    add missing field and adjust to logical order

--- src/containers/Golf/GolfPage/children/course/CourseSummary.js
+++ src/containers/Golf/GolfPage/children/course/CourseSummary.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 import HoleTable from '@golf/GolfPage/children/hole/HoleTable';
+import {
+    FlexContainer,
+    FlexItemLabel,
+    FlexItemValue
+} from '@containers/Golf/styles/layout.style';
 
 const CourseSummary = ({ course }) => {
+
     return (
         <div>
+            <header className="c-header nudge">Course</header>
+            <div className="c-box">
+                <FlexContainer>
+                    <FlexItemLabel>Name</FlexItemLabel>
+                    <FlexItemValue>{ `${ course.courseName.value }`}</FlexItemValue>
+                </FlexContainer>
+                <FlexContainer>
+                    <FlexItemLabel>Slope</FlexItemLabel>
+                    <FlexItemValue>{ `${ course.courseSlope.value }`}</FlexItemValue>
+                </FlexContainer>
+            </div>
             <HoleTable holes={ course.courseHoles.value }/>
         </div>
     );
