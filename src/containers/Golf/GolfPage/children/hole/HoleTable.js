70289763e240bd0cef8f315437caba70638787b3
Michaud <michaud@venant.nl>
2020-02-29 02:58:24 +0100

    add marker score and labels

--- src/containers/Golf/GolfPage/children/hole/HoleTable.js
+++ src/containers/Golf/GolfPage/children/hole/HoleTable.js
@@ -17,11 +17,13 @@ const HoleTable = ({
     const outLengths = outHoles.map(hole => hole.holeLength.value);
     const outStrokeIndices = outHoles.map(hole => hole.holeStrokeIndex.value);
     const outGameStrokes = outHoles.map(hole => hole.gameStrokes ? hole.gameStrokes.value.length > 0 ? hole.gameStrokes.value.length : '' : '');
+    const outGameMarkerStrokes = outHoles.map(hole => hole.gameMarkerStrokeCount.value);
     const inHoleNumbers = inHoles.map(hole => hole.holeNumber.value);
     const inPars = inHoles.map(hole => hole.holePar.value);
     const inLengths = inHoles.map(hole => hole.holeLength.value);
     const inStrokeIndices = inHoles.map(hole => hole.holeStrokeIndex.value);
     const inGameStrokes = inHoles.map(hole => hole.gameStrokes ? hole.gameStrokes.value.length > 0 ? hole.gameStrokes.value.length : '' : '');
+    const inGameMarkerStrokes = inHoles.map(hole => hole.gameMarkerStrokeCount.value);
 
     return (
         <div className="c-box">
@@ -32,19 +34,23 @@ const HoleTable = ({
                         { outHoleNumbers.map((nr, index) => <th className="hole-table__header--button" onClick={ editHoleHandler(index) } key={ index }><span>{ nr }</span></th>) }
                         { outHolesEmpty.map((nr, index) => <th className="hole-table__header--empty" key={ index }><span></span></th>) }
                     </tr>
-                    <tr>
+                    <tr className="t-row--par">
                         { outPars.map((p, index) => <td key={ index }>{ p }</td>) }
                         { outHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
-                    <tr>
-                        { outGameStrokes.map((s, index) => <td key={ index }>{ s }</td>) }
+                    <tr className="t-row--player-score">
+                        { outGameStrokes.map((s, index) => <td className="hole-table__cell--score" key={ index }>{ s }</td>) }
                         { outHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
-                    <tr>
+                    <tr className="t-row--marker-score">
+                        { outGameMarkerStrokes.map((s, index) => <td className="hole-table__cell--score" key={ index }>{ s }</td>) }
+                        { outHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
+                    </tr>
+                    <tr className="t-row--si">
                         { outStrokeIndices.map((s, index) => <td className={ s === 0 ? 'invalid' : '' } key={ index }>{ s }</td>) }
                         { outHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
-                    <tr>
+                    <tr className="t-row--length">
                         { outLengths.map((l, index) => <td key={ index }>{ l }</td>) }
                         { outHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
@@ -60,8 +66,12 @@ const HoleTable = ({
                         { inPars.map((p, index) => <td key={ index }>{ p }</td>) }
                         { inHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
+                    <tr className="t-row--player-score">
+                        { inGameStrokes.map((s, index) => <td className="hole-table__cell--score" key={ index }>{ s }</td>) }
+                        { inHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
+                    </tr>
                     <tr>
-                        { inGameStrokes.map((s, index) => <td key={ index }>{ s }</td>) }
+                        { inGameMarkerStrokes.map((s, index) => <td className="hole-table__cell--score" key={ index }>{ s }</td>) }
                         { inHolesEmpty.map((nr, index) => <td className="hole-table__cell--empty" key={ index }><span></span></td>) }
                     </tr>
                     <tr>
