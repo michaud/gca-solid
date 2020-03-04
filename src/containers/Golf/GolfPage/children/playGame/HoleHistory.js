6cd03afa2482352dbc88233d64b1d06b29042b1e
Michaud <michaud@venant.nl>
2020-03-04 13:40:19 +0100

    add stroke distance to stroke detail

--- src/containers/Golf/GolfPage/children/playGame/HoleHistory.js
+++ src/containers/Golf/GolfPage/children/playGame/HoleHistory.js
@@ -1,53 +1,8 @@
 import React, { useState } from 'react';
 
-import StrokeDetail from './StrokeDetail';
+import calculateStrokeDistance from '@golfutils/calculateStrokeDistance';
 
-const getDistance = (lat1, lon1, lat2, lon2, unit) => {
-
-    if ((lat1 === lat2) && (lon1 === lon2)) return 0;
-
-    const radlat1 = Math.PI * lat1/180;
-    const radlat2 = Math.PI * lat2/180;
-    const theta = lon1-lon2;
-    const radtheta = Math.PI * theta/180;
-    let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-    if (dist > 1) dist = 1;
-
-    dist = Math.acos(dist);
-    dist = dist * 180/Math.PI;
-    dist = dist * 60 * 1.1515;
-    if (unit === "K") { dist = dist * 1.609344 }
-    if (unit === "N") { dist = dist * 0.8684 }
-
-        return dist;
-};
-
-const calculateStrokeDistanceInMeters = (stroke, hole) => {
-
-    if(!hole) return 0;
-    if(!stroke) return 0;
-
-    const currStrokeIndex = hole.gameStrokes.value.indexOf(stroke); 
-    const nextStrokeIndex = currStrokeIndex !== -1 && currStrokeIndex + 1 < hole.gameStrokes.value.length ? currStrokeIndex + 1 : -1;
-
-    if(nextStrokeIndex !== -1) {
-        
-        const nextStroke = hole.gameStrokes.value[nextStrokeIndex];
-
-        return Math.round(
-            getDistance(
-                stroke.strokeLocation.value.latitude.value,
-                stroke.strokeLocation.value.longitude.value,
-                nextStroke.strokeLocation.value.latitude.value,
-                nextStroke.strokeLocation.value.longitude.value,
-                'K'
-            ) * 1000);
-
-    } else {
-
-        return 0;
-    }
-};
+import StrokeDetail from '@golf/GolfPage/children/playGame/StrokeDetail';
 
 const HoleHistory = ({ hole, onSelect, onDeleteStroke }) => {
 
@@ -56,7 +11,12 @@ const HoleHistory = ({ hole, onSelect, onDeleteStroke }) => {
     const onSelectHandler = (stroke) => () => {
 
         onSelect && onSelect(stroke);
-        setSelectedStroke(stroke);
+        if(selectedStroke && selectedStroke.iri === stroke.iri) {
+            setSelectedStroke();
+        } else {
+            setSelectedStroke(stroke);
+        }
+        
     };
 
     const onDeleteStrokeHandler = (stroke) => {
@@ -65,7 +25,7 @@ const HoleHistory = ({ hole, onSelect, onDeleteStroke }) => {
         setSelectedStroke();
     }
 
-    const distance = calculateStrokeDistanceInMeters(selectedStroke, hole);
+    const distance = calculateStrokeDistance(selectedStroke, hole);
 
     return (
         <>
---
+++ src/containers/Golf/utils/calculateStrokeDistance.js
@@ -0,0 +1,61 @@
+const degToRad = ( deg ) => {
+    return Math.PI * deg / 180;
+};
+
+const getDistance = (lat1, lon1, lat2, lon2, unit) => {
+
+    if ((lat1 === lat2) && (lon1 === lon2)) return 0;
+
+    const radLat1 = degToRad(lat1);
+    const radLat2 = degToRad(lat2);
+    const theta = lon1 - lon2;
+    const radTheta = degToRad(theta);
+
+    let extent = Math.sin(radLat1) *
+        Math.sin(radLat2) +
+        Math.cos(radLat1) *
+        Math.cos(radLat2) *
+        Math.cos(radTheta);
+
+    const length = extent > 1 ? 1 : Math.acos(extent) * 180 / Math.PI * 60;
+
+    switch(unit) {
+
+        case 'y' : {
+            return (length * 1.1515 * 1760);
+        }
+
+        default: {
+
+            return (length * 1.853159616 * 1000);
+        }
+    }
+};
+
+const calculateStrokeDistance = (stroke, hole) => {
+    
+    if(!hole) return 0;
+    if(!stroke) return 0;
+
+    const currStrokeIndex = hole.gameStrokes.value.indexOf(stroke); 
+    const nextStrokeIndex = currStrokeIndex !== -1 && currStrokeIndex + 1 < hole.gameStrokes.value.length ? currStrokeIndex + 1 : -1;
+
+    if(nextStrokeIndex !== -1) {
+        
+        const nextStroke = hole.gameStrokes.value[nextStrokeIndex];
+
+        return Math.round(
+            getDistance(
+                stroke.strokeLocation.value.latitude.value,
+                stroke.strokeLocation.value.longitude.value,
+                nextStroke.strokeLocation.value.latitude.value,
+                nextStroke.strokeLocation.value.longitude.value
+            ));
+
+    } else {
+
+        return 0;
+    }
+};
+
+export default calculateStrokeDistance;
