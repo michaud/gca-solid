f92892c733494039b32b4243585cef54e30b8554
Michaud <michaud@venant.nl>
2020-03-09 16:42:45 +0100

    remove over eager selector

--- src/containers/Golf/styles/page.style.js
+++ src/containers/Golf/styles/page.style.js
@@ -4,15 +4,6 @@ export const PageContainer = styled.div`
     padding: 1rem 1rem 2rem 1rem;
     overflow-y: scroll;
     height: calc(100vh - 10.8rem);
-
-    > * {
-
-        margin-bottom: 2rem;
-
-        &:last-child {
-            margin-bottom: 0;
-        }
-    }
 `;
 
 export const PageContent = styled.div`
