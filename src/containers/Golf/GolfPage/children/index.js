87c0c98067f7b73420ad5838b67c12ee2bf66ef5
Michaud <michaud@venant.nl>
2022-02-03 10:56:11 +0100

    clean up unused

--- src/containers/Golf/GolfPage/children/index.js
+++ src/containers/Golf/GolfPage/children/index.js
@@ -1,5 +1,4 @@
 import BagTransferList from './bag/BagTransferList';
-import ButtonBar from './ButtonBar';
 import ClubDetail from './club/ClubDetail';
 import ClubForm from './club/ClubForm';
 import ClubList from './club/ClubList';
@@ -8,29 +7,25 @@ import CourseDetail from './course/CourseDetail';
 import CourseForm from './course/CourseForm';
 import HoleTable from './hole/HoleTable';
 import CourseList from './course/CourseList';
-import formStyles from '@styles/form.style';
 import GolfApp from './GolfApp';
 import HoleDetail from './hole/HoleDetail';
 import HoleForm from './hole/HoleForm';
 import HoleList from './hole/HoleList';
-import IDEditor from './IDEditor';
 import ManageBag from './bag/ManageBag';
 import ManageCourses from './course/ManageCourses';
 import ManageHoles from './hole/ManageHoles';
 import ManageMarkers from './player/ManageMarkers';
 import ManagePlayers from './player/ManagePlayers';
 import MarkerList from './player/MarkerList';
-import ModuleHeader from './ModuleHeader';
-import NavigationShell from './NavigationShell';
 import PlayerDetail from './player/PlayerDetail';
 import PlayerForm from './player/PlayerForm';
 import PlayGame from './playGame/PlayGame';
-import SplashScreen from './SplashScreen';
+import SplashScreenLoading from './splash/SplashScreenLoading';
+import SplashScreen from './splash/SplashScreen';
 import PlayingHandicapDetail from './game/PlayingHandicapDetail'
 
 export {
     BagTransferList,
-    ButtonBar,
     ClubDetail,
     ClubForm,
     ClubList,
@@ -39,23 +34,20 @@ export {
     CourseForm,
     HoleTable,
     CourseList,
-    formStyles,
     GolfApp,
     HoleDetail,
     HoleForm,
     HoleList,
-    IDEditor,
     ManageBag,
     ManageCourses,
     ManageHoles,
     ManageMarkers,
     ManagePlayers,
     MarkerList,
-    ModuleHeader,
-    NavigationShell,
     PlayerDetail,
     PlayerForm,
     SplashScreen,
+    SplashScreenLoading,
     PlayGame,
     PlayingHandicapDetail
 }
